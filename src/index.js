0806c9ac0bfc56ef15f488812672de9abd792470
AlexGR0 <it_zd1026265758@qq.com>
2023-05-21 16:59:28 +0800

    feat: 安装及配置babel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 function sum(a, b) {
   return a + b;
-}
+};
 var result = sum(12, 23);
+const aaa = () => {
+    console.log('测试箭头函数');
+};
+aaa();
 console.log(result);
--- webpack-config/webpack.common.js
+++ webpack-config/webpack.common.js
@@ -1,10 +1,20 @@
-const {join, resolve} = require('path');
+const { join, resolve } = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: resolve('./src/index'),
-  output:{
-    filename: 'bundle.js',
-    path: resolve(__dirname, '../dist')
-  }
+  mode: "development",
+  entry: resolve("./src/index"),
+  output: {
+    filename: "bundle.js",
+    path: resolve(__dirname, "../dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts|jsx|tsx)$/,
+        include: [resolve("src")],
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
 };
