3176cef12149f4349869a6d8e750cddb040b9932
AlexGR0 <it_zd1026265758@qq.com>
2023-05-31 22:24:07 +0800

    feat: 构建耗时

---
+++ webpack-config/webpack.analy.js
@@ -0,0 +1,7 @@
+const prodConfig = require("./webpack.prod");
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const smp = new SpeedMeasurePlugin();
+const { merge } = require("webpack-merge");
+
+// 预留合并空位加入自定义配置对比
+module.exports = smp.wrap(merge(prodConfig, {}));
